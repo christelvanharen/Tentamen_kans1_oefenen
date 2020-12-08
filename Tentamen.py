3ae7f3a35ec87d52dd3b9883b28802c1bfef8d52
christelvanharen <cvanharen@outlook.com>
2020-12-08 12:32:52 +0100

    grafiek y-as en begin gui

--- Tentamen.py
+++ Tentamen.py
@@ -1,6 +1,8 @@
 import re
 import numpy as np
 import matplotlib.pyplot as plt
+import tkinter
+from tkinter.ttk import Combobox
 
 def main():
     fasta = "TAIR10_pep_20101214.fa"
@@ -13,6 +15,7 @@ def main():
     # print(data)
     info_alles = gff_headers(headers_voldaan, data)
     grafiek(info_alles)
+    Gui(info_alles)
 
 def lees_fasta(bestand):
     try:
@@ -102,15 +105,27 @@ def gff_headers(headers_voldoen, data):
 
 def grafiek(info_alles):
     x_as = []
+    y_as = []
     for onderdeel_lijst in info_alles:
         # print(onderdeel_lijst[1])
         if onderdeel_lijst[1] not in x_as:
             # print(onderdeel_lijst[1])
             x_as.append(onderdeel_lijst[1])
-    print(x_as)
+        y_as.append(onderdeel_lijst[1])
+    print("X-as", x_as)
+    print("Y-as", y_as)
+
+    chr1 = y_as.count("Chr1")
+    chr2 = y_as.count("Chr2")
+    chr3 = y_as.count("Chr3")
+    chr4 = y_as.count("Chr4")
+    chr5 = y_as.count("Chr5")
+
+    print(chr1, chr2, chr3, chr4, chr5)
+
 
     # Make a fake dataset:
-    height = [3, 12, 5, 18, 45]
+    height = [chr1, chr2, chr3, chr4, chr5]
     y_pos = np.arange(len(x_as))
 
     # Create bars
@@ -118,9 +133,53 @@ def grafiek(info_alles):
 
     # Create names on the x-axis
     plt.xticks(y_pos, x_as)
+    plt.title("Het aantal genen met Serine/Threonine kinase active "
+              "site per chromosoom")
+    plt.xlabel("Chromosoom nummer")
+    plt.ylabel("Aantal genen met Serine/Threonine kinase active site")
 
     # Show graphic
     plt.show()
 
+class Gui:
+
+    def __init__(self, info_alles):
+        # maakt een window en geeft een titel
+        self.lijst = info_alles
+
+        self.main_window = tkinter.Tk()
+        self.main_window.title("Uitleg")
+
+        # maakt Frames ( gedeeltes in de main_window).
+        self.top_frame = tkinter.Frame(self.main_window)
+        self.mid_frame = tkinter.Frame(self.main_window)
+        self.bottom_frame = tkinter.Frame(self.main_window)
+
+        #lijst in gui
+
+        self.drop = Combobox(self.bottom_frame,
+                             values = self.lijst)
+
+        self.drop.set("Select")
+
+        self.drop.pack()
+
+        # knop die functie doe iets aanroept
+        self.ok_button = tkinter.Button(self.bottom_frame,
+                                        text='OK',
+                                        command=self.doe_iets)
+        self.ok_button.pack()
+
+        # pack Frames (zichtbaar maken frames in main window)
+        self.top_frame.pack()
+        self.mid_frame.pack()
+        self.bottom_frame.pack()
+
+        #zorg dat Gui 'aan' gaat
+        tkinter.mainloop()
+
+    def doe_iets(self):
+        entry = self.drop.get()
+        print(entry)
 
 main()
